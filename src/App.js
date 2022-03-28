86899d35f55253b6bca94c6349c7c1091341ae77
anannanur <anannaanu974@gmail.com>
2022-03-28 14:25:58 +0600

    card dynamically added

--- src/App.js
+++ src/App.js
@@ -5,17 +5,21 @@ import Cardgroup from './Components/Cardgroup/Cardgroup';
 function App() {
   return (
     <div className="App">
-      <Cardgroup></Cardgroup>
-      <button type="button" className="btn btn-primary">Primary</button>
-      <button type="button" className="btn btn-secondary">Secondary</button>
-      <button type="button" className="btn btn-success">Success</button>
-      <button type="button" className="btn btn-danger">Danger</button>
-      <button type="button" className="btn btn-warning">Warning</button>
-      <button type="button" className="btn btn-info">Info</button>
-      <button type="button" className="btn btn-light">Light</button>
-      <button type="button" className="btn btn-dark">Dark</button>
 
-      <button type="button" className="btn btn-link">Link</button>
+      <div>
+        <button type="button" className="btn btn-primary">Primary</button>
+        <button type="button" className="btn btn-secondary">Secondary</button>
+        <button type="button" className="btn btn-success">Success</button>
+        <button type="button" className="btn btn-danger">Danger</button>
+        <button type="button" className="btn btn-warning">Warning</button>
+        <button type="button" className="btn btn-info">Info</button>
+        <button type="button" className="btn btn-light">Light</button>
+        <button type="button" className="btn btn-dark">Dark</button>
+
+        <button type="button" className="btn btn-link">Link</button>
+      </div>
+
+      <Cardgroup></Cardgroup>
     </div>
   );
 }
--- src/Components/Cardgroup/Cardgroup.js
+++ src/Components/Cardgroup/Cardgroup.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Card from '../Card/Card';
 
 const Cardgroup = () => {
+    const products = [
+        { id: 1, name: 'laptop', price: 187000 },
+        { id: 2, name: 'laptop max', price: 124000 },
+        { id: 3, name: 'laptop grow', price: 87000 }
+    ]
     return (
         <div>
-            <h1>Hi!! from CardGroup</h1>
+            <h1 className="py-3">Hi!! from CardGroup</h1>
+            <div className="row row-cols-1 row-cols-md-3 g-5 mx-3">
+                {
+                    products.map(product => <Card key={product.id} card={product}></Card>)
+                }
+            </div>
         </div>
     );
 };
