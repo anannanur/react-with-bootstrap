7342373894862bb5986f714c422c57a0de09069e
anannanur <anannaanu974@gmail.com>
2022-03-28 13:08:44 +0600

    added bootstrap

---
+++ src/Components/Cardgroup/Cardgroup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cardgroup = () => {
+    return (
+        <div>
+            <h1>Hi!! from CardGroup</h1>
+        </div>
+    );
+};
+
+export default Cardgroup;
